16482d139f184d35a027316f16b25b27e28e4ae0
Zoltán Arnóczky <84664281+azolee1848@users.noreply.github.com>
2022-04-10 05:22:14 +0200

    Menu system ready.

--- script/script.js
+++ script/script.js
@@ -1,15 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
-     let menu = document.getElementById('menu');
-     let hideMenuButton = document.getElementById('menu-toggle-button');
-     let initialWidthOfTheMenu = getComputedStyle(menu).width;
- 
-     setTimeout(function(){menu.style.width = '0';},2000);
- 
-     hideMenuButton.onclick = function () {
-         if (parseInt(getComputedStyle(menu).width,10) > 0) {
-             menu.style.width = '0';
-         } else {
-             menu.style.width = initialWidthOfTheMenu;
-         }
-     }; 
+    let menu = document.getElementById('menu');
+    let hideMenuButton = document.getElementById('menu-toggle-button');
+    let initialWidthOfTheMenu = getComputedStyle(menu).width;
+    
+    setTimeout(function(){
+        menu.style.width = '0';
+    },2000);
+
+    hideMenuButton.onclick = function () {
+        if (parseInt(getComputedStyle(menu).width,10) > 0) {
+            menu.style.width = '0';
+        } else {
+            menu.style.width = initialWidthOfTheMenu;
+        }
+    }; 
  });
