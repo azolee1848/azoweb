3f30f87c156676a298cbd15d8fd75ba3e85c0d26
Zoltán Arnóczky <84664281+azolee1848@users.noreply.github.com>
2022-04-23 10:10:45 +0200

    Dashboard panels got correct sizing, animation, and made clickable.
    Resizing method may be change.

--- script/script.js
+++ script/script.js
@@ -1,17 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let menu = document.getElementById('menu');
-    let hideMenuButton = document.getElementById('menu-toggle-button');
-    let initialWidthOfTheMenu = getComputedStyle(menu).width;
-    
-    setTimeout(function(){
-        menu.style.width = '0';
-    },2000);
+document.addEventListener("DOMContentLoaded", function () {
+  let menu = document.getElementById("menu");
+  let hideMenuButton = document.getElementById("menu-toggle-button");
+  let initialWidthOfTheMenu = getComputedStyle(menu).width;
 
-    hideMenuButton.onclick = function () {
-        if (parseInt(getComputedStyle(menu).width,10) > 0) {
-            menu.style.width = '0';
-        } else {
-            menu.style.width = initialWidthOfTheMenu;
-        }
-    }; 
- });
+  setTimeout(function () {
+    menu.style.width = "0";
+  }, 2000);
+
+  hideMenuButton.onclick = function () {
+    if (parseInt(getComputedStyle(menu).width, 10) > 0) {
+      menu.style.width = "0";
+    } else {
+      menu.style.width = initialWidthOfTheMenu;
+    }
+  };
+
+  document.getElementById("dashboardLeftTopPolygon").onclick = function () {
+    console.log("Left top.");
+  };
+
+  document.getElementById("dashboardRightTopPolygon").onclick = function () {
+    console.log("Right top.");
+  };
+  document.getElementById("dashboardLeftBottomPolygon").onclick = function () {
+    console.log("Left bottom.");
+  };
+  document.getElementById("dashboardRightBottomPolygon").onclick = function () {
+    console.log("Right bottom.");
+  };
+});
