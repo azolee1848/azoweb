fc033d1fd1dda02f77199d5770f95d77720c2fb0
Arnóczky Zoltán <azolee1848@gmail.com>
2022-07-08 22:59:24 +0200

    Huge refactoring done. Footer element and typescript logo added.

--- script/script.js
+++ script/script.js
@@ -1,37 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let menu = document.getElementById("menu");
-  let hideMenuButton = document.getElementById("menu-toggle-button");
-  let initialWidthOfTheMenu = getComputedStyle(menu).width;
-
-  setTimeout(function () {
-    menu.style.width = "0";
-  }, 2000);
-
-  hideMenuButton.onclick = function () {
-    if (parseInt(getComputedStyle(menu).width, 10) > 0) {
-      menu.style.width = "0";
-    } else {
-      menu.style.width = initialWidthOfTheMenu;
-    }
-  };
-
-  document.getElementById("dashboardLeftTopPolygon").onclick = function () {
+  document.getElementById("dashboard__left-top-polygon").onclick = function () {
     window.location.href = "technologies.html";
   };
 
-  document.getElementById("dashboardRightTopPolygon").onclick = function () {
-    window.location.href = "skills.html";
-  };
+  document.getElementById("dashboard__right-top-polygon").onclick =
+    function () {
+      window.location.href = "skills.html";
+    };
 
-  document.getElementById("callForActionCenterImage").onclick = function () {
-    window.open("mailto:info@azoweb.hu?subject=Frontend work");
-  };
+  document.getElementById("dashboard__call-for-action-image").onclick =
+    function () {
+      window.open("mailto:info@azoweb.hu?subject=Frontend work");
+    };
 
-  document.getElementById("dashboardLeftBottomPolygon").onclick = function () {
-    window.location.href = "business.html";
-  };
+  document.getElementById("dashboard__left-bottom-polygon").onclick =
+    function () {
+      window.location.href = "business.html";
+    };
 
-  document.getElementById("dashboardRightBottomPolygon").onclick = function () {
-    window.location.href = "contact.html";
-  };
+  document.getElementById("dashboard__right-bottom-polygon").onclick =
+    function () {
+      window.location.href = "contact.html";
+    };
 });
