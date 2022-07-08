b58de486b94bd1425b96c3aa193d74d9c6d192f7
Arnóczky Zoltán <azolee1848@gmail.com>
2022-07-08 23:40:19 +0200

    Minor bug fixed.

--- script/script.js
+++ script/script.js
@@ -1,25 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("dashboard__left-top-polygon").onclick = function () {
-    window.location.href = "technologies.html";
-  };
+document.addEventListener("readystatechange", function () {
+  if (document.readyState === "complete") {
+    document.getElementById("dashboard__left-top-polygon").onclick =
+      function () {
+        window.location.href = "technologies.html";
+      };
 
-  document.getElementById("dashboard__right-top-polygon").onclick =
-    function () {
-      window.location.href = "skills.html";
-    };
+    document.getElementById("dashboard__right-top-polygon").onclick =
+      function () {
+        window.location.href = "skills.html";
+      };
 
-  document.getElementById("dashboard__call-for-action-image").onclick =
-    function () {
-      window.open("mailto:info@azoweb.hu?subject=Frontend work");
-    };
+    document.getElementById("dashboard__call-for-action-image").onclick =
+      function () {
+        window.open("mailto:info@azoweb.hu?subject=Frontend work");
+      };
 
-  document.getElementById("dashboard__left-bottom-polygon").onclick =
-    function () {
-      window.location.href = "business.html";
-    };
+    document.getElementById("dashboard__left-bottom-polygon").onclick =
+      function () {
+        window.location.href = "business.html";
+      };
 
-  document.getElementById("dashboard__right-bottom-polygon").onclick =
-    function () {
-      window.location.href = "contact.html";
-    };
+    document.getElementById("dashboard__right-bottom-polygon").onclick =
+      function () {
+        window.location.href = "contact.html";
+      };
+  }
 });
